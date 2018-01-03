512a7799090d50b94b1d818bc07c4db9a7b06414
philmora <philnz77@gmail.com>
2018-01-03 17:09:37 +1300

    minimal change to fix crash, appears that you need to have a reducer for board or else combineReducers will blank out any initial state you set (that is not represented by an entry in the combineReducers arg)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import SelectSquare1 from './reducer-select-square';
-
+import Board from './reducer-board';
 const checkersApp = combineReducers({
-  selectSquare: SelectSquare1
+  selectSquare: SelectSquare1,
+  board: Board
   })
-  
+
   export default checkersApp
